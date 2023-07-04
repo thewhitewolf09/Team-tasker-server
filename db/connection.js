cf620bc6036ef4b1081df878b7c2cdee27005496
thewhitewolf09 <sumitramprakashnirmal@gmail.com>
2023-07-04 09:53:46 +0530

    first commit

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const DB = process.env.DATABASE
 
-mongoose.connect(DB).then(() => {
+mongoose.connect(DB,{ useNewUrlParser: true }).then(() => {
     console.log('Connection successful');
 }).catch((err) => {
     console.log(err);
